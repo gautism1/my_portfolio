d2364f1f8a6f1c61252c446f36739b0eed6198ee
bhavesh27 <bhaveshkumar19856@gmail.com>
2020-06-03 10:23:04 +0530

    restructured and added loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ErrorBoundary from './HOC/ErrorBoundary';
-import './App.css';
+import ErrorBoundary from "./HOC/ErrorBoundary";
+import Loader from "./Loader";
+import Nav from "./Nav";
+import Footer from "./footer";
+import "./App.css";
 
-
-const HeroHeader = lazy(() => import('./HeroHeader'));
-const Projects = lazy(() => import('./Projects'));
-const Contact = lazy(() => import('./Contact'));
-const Skills = lazy(() => import('./Skills'));
-const Nav = lazy(() => import('./Nav'));
-const Footer = lazy(() => import('./footer'));
+const HeroHeader = lazy(() => import("./HeroHeader"));
+const Projects = lazy(() => import("./Projects"));
+const Contact = lazy(() => import("./Contact"));
+const Skills = lazy(() => import("./Skills"));
 
 function App() {
   const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  
   React.useEffect(() => {
     localStorage.setItem("dark", JSON.stringify(darkMode));
-  }, [darkMode ]);
+  }, [darkMode]);
+  
   function getInitialMode() {
     const isReturningUser = "dark" in localStorage;
     const savedMode = JSON.parse(localStorage.getItem("dark"));
@@ -32,28 +34,34 @@ function App() {
     if (!window.matchMedia) return;
     return window.matchMedia("(prefers-color-scheme: dark)").matches;
   }
+
   return (
-    <Router>
-      <ErrorBoundary>
-        <Suspense fallback={<h1>Loading Web App...</h1>}>
-          <div className="App">
-            <div className={darkMode ? "main dark-mode" : " main light-mode"}>
-              <Nav setDarkMode={setDarkMode} darkMode={darkMode}/>
+    <ErrorBoundary>
+      <Suspense fallback={<h1>Loading Web App...</h1>}>
+        <div className="App">
+          <div className={darkMode ? "main dark-mode" : " main light-mode"}>
+            <Router>
+              <Nav setDarkMode={setDarkMode} darkMode={darkMode} />
               <div className="this">
-                <Switch>
-                  <Route path="/" exact component={HeroHeader} />
-                  <Route path="/Skills" component={Skills} />
-                  <Route path="/Projects" component={Projects} />
-                  <Route path="/Contact" component={Contact} />
-                  <Route render={() => <p className="link">Not found</p>} />
-                </Switch>
+                <Suspense fallback={<Loader />}>
+                  <Switch>
+                    <Route exact path="/" component={HeroHeader} />
+                    <Route path="/Skills" component={Skills} />
+                    <Route path="/Projects" component={Projects} />
+                    <Route path="/Contact" component={Contact} />
+                    <Route
+                      path="*"
+                      render={() => <p className="link">Not found</p>}
+                    />
+                  </Switch>
+                </Suspense>
               </div>
-              <Footer />
-            </div>
+            </Router>
+            <Footer />
           </div>
-        </Suspense>
-      </ErrorBoundary>
-    </Router>
+        </div>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 export default App;
